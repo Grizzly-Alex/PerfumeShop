e2f0bffd71d3168f46458256b38c3467a12dde64
Alexander Medved <medved1991alexander@gmail.com>
2023-03-29 14:20:49 +0300

    Add DbContext

---
+++ PerfumeShop/PerfumeShop.Infrastructure/DataAccess/DbContexts/CatalogDbContext .cs
@@ -0,0 +1,12 @@
+﻿namespace PerfumeShop.Infrastructure.DataAccess.DbContexts;
+
+public sealed class CatalogDbContext : DbContext
+{
+
+    public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //TODO
+    }
+}
