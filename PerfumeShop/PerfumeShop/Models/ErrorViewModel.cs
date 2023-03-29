a8cdeb6e68c93e64d67e21f0d2e64a7167702b40
Alexander Medved <medved1991alexander@gmail.com>
2023-03-29 11:47:37 +0300

    Created project

---
+++ PerfumeShop/PerfumeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PerfumeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
