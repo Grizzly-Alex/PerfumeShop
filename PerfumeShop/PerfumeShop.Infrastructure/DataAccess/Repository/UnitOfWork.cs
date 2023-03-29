a28abb77ebe3a22f114a6c80f919a3acb363b85f
Alexander Medved <medved1991alexander@gmail.com>
2023-03-29 17:58:46 +0300

    Implement IUnitOfWork

---
+++ PerfumeShop/PerfumeShop.Infrastructure/DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace PerfumeShop.Infrastructure.DataAccess.Repository;
+
+public sealed class UnitOfWork<TDbContext> : IUnitOfWork<TDbContext>
+    where TDbContext : DbContext
+{
+    private readonly TDbContext _db;
+
+    public UnitOfWork(TDbContext db) => _db = db;
+
+    public async Task SaveChangesAsync() => await _db.SaveChangesAsync();
+
+    public IRepository<TDbContext, TEntity> GetRepository<TEntity>()
+        where TEntity : Entity
+        => new Repository<TDbContext, TEntity>(_db);
+}
