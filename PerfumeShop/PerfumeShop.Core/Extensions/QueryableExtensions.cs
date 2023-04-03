83bdf31ceacca06758ceba65e4d6ec80e8b2dafb
Alexander Medved <medved1991alexander@gmail.com>
2023-04-03 12:54:24 +0300

    Add QueryableExtensions

---
+++ PerfumeShop/PerfumeShop.Core/Extensions/QueryableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace PerfumeShop.Core.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<IPagedList<TItem>> ToPagedListAsync<TItem>(this IQueryable<TItem> source,
+        int pageIndex, int itemsPerPage, CancellationToken cancellationToken = default)
+    {
+        var totalItems = await source
+            .CountAsync(cancellationToken)
+            .ConfigureAwait(false);
+        var items = await source
+            .Skip(pageIndex * itemsPerPage)
+            .Take(itemsPerPage)
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        return new PagedList<TItem>(items, pageIndex, itemsPerPage, totalItems);
+    }
+}
