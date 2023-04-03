49de58c22fbe7cedf47bc06baaf4207a73327c67
Alexander Medved <medved1991alexander@gmail.com>
2023-04-03 12:51:48 +0300

    Add PagedList

---
+++ PerfumeShop/PerfumeShop.Core/Interfaces/IPagedList.cs
@@ -0,0 +1,12 @@
+﻿namespace PerfumeShop.Core.Interfaces;
+
+public interface IPagedList<TItem>
+{
+    public int PageId { get; }
+    public int TotalPages { get; }
+    public int ItemsPerPage { get; }
+    public int TotalItems { get; }
+    public bool HasPreviousPage { get; }
+    public bool HasNextPage { get; }
+    public IList<TItem> Items { get; }
+}
---
+++ PerfumeShop/PerfumeShop.Core/Models/PagedList.cs
@@ -0,0 +1,23 @@
+﻿namespace PerfumeShop.Core.Models;
+
+public sealed class PagedList<TItem> : IPagedList<TItem>
+{
+    public int PageId { get; init; }
+    public int TotalPages { get; init; }
+    public int ItemsPerPage { get; init; }
+    public int TotalItems { get; init; }
+    public bool HasPreviousPage => PageId > 0;
+    public bool HasNextPage => PageId + 1 < TotalPages;
+    public IList<TItem> Items { get; init; }
+
+    public PagedList(IEnumerable<TItem> items, int pageId, int itemsPerPage, int totalItems)
+    {
+        Items = items.ToList();
+        PageId = pageId;
+        ItemsPerPage = itemsPerPage;
+        TotalItems = totalItems;
+        TotalPages = (int)Math.Ceiling(totalItems / (double)ItemsPerPage);
+    }
+
+    public PagedList() => Items = Array.Empty<TItem>();
+}
