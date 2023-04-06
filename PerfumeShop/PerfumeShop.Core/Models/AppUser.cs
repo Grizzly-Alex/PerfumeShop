00284655edd15d8e0b8828b4fa1ff40661fb28bf
Alexander Medved <medved1991alexander@gmail.com>
2023-04-06 22:40:05 +0300

    Add regiter view page

--- PerfumeShop/PerfumeShop.Core/Models/AppUser.cs
+++ PerfumeShop/PerfumeShop.Core/Models/AppUser.cs
@@ -2,10 +2,10 @@
 
 public sealed class AppUser : IdentityUser
 {
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public string? StreetAddress { get; private set; }
-    public string? City { get; private set; }
-    public string? State { get; private set; }
-    public string? PostalCode { get; private set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
 }
--- PerfumeShop/PerfumeShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PerfumeShop/PerfumeShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -34,8 +34,38 @@ public class RegisterModel : PageModel
 
     public class InputModel
     {
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Street Address")]
+        public string? StreetAddress { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "City")]
+        public string? City { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "State")]
+        public string? State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [Display(Name = "Postal Code")]
+        public string? PostalCode { get; set; }
+
+        [Phone]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
         [Required]
         [EmailAddress]
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
         public string? Email { get; set; }
 
@@ -61,7 +91,18 @@ public class RegisterModel : PageModel
         returnUrl ??= Url.Content("~/");
         if (ModelState.IsValid)
         {
-            var user = new AppUser { UserName = Input.Email, Email = Input.Email };
+            var user = new AppUser
+            {
+                UserName = Input.Email,
+                FirstName = Input.FirstName,
+                LastName = Input.LastName,
+                StreetAddress = Input.StreetAddress,
+                City = Input.City,
+                State = Input.State,
+                PostalCode = Input.PostalCode,
+                PhoneNumber = Input.PhoneNumber,
+                Email = Input.Email,               
+            };
             var result = await _userManager.CreateAsync(user, Input.Password);
             if (result.Succeeded)
             {
