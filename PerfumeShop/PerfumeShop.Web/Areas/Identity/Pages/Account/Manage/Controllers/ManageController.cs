433183940e7076004a9d6ff8af575e394631fe33
Alexander Medved <medved1991alexander@gmail.com>
2023-04-07 12:51:16 +0300

    Add IndexUserViewModel

---
+++ PerfumeShop/PerfumeShop.Web/Areas/Identity/Pages/Account/Manage/Controllers/ManageController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Encodings.Web;
+
+namespace PerfumeShop.Web.Areas.Identity.Pages.Account.Manage.Controllers;
+
+public class ManageController : Controller
+{
+    [TempData]
+    public string? StatusMessage { get; set; }
+
+    private readonly ILogger<ManageController> _logger;
+    private readonly IEmailSender _emailSender;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly UrlEncoder _urlEncoder;
+
+    private const string RecoveryCodesKey = nameof(RecoveryCodesKey);
+    private const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
+
+    public ManageController(
+        ILogger<ManageController> logger,
+        IEmailSender emailSender,
+        UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager,
+        UrlEncoder urlEncoder)
+    {
+        _logger = logger;
+        _emailSender = emailSender;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _urlEncoder = urlEncoder;
+    }
+    public async Task<IActionResult> MyAccount()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user is null)
+        {
+            throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        }
+
+        var model = new IndexViewModel
+        {
+            Username = user.UserName,
+            Email = user.Email,
+            PhoneNumber = user.PhoneNumber,
+            IsEmailConfirmed = user.EmailConfirmed,
+            StatusMessage = StatusMessage
+        };
+
+        return View();
+    }
+}
---
+++ PerfumeShop/PerfumeShop.Web/ViewModels/IndexUserViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace PerfumeShop.Web.ViewModels;
+
+public class IndexUserViewModel
+{
+    public string UserName { get; set; }
+    public string? StatusMessage { get; set; }
+    public bool IsEmailConfirmed { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "First Name")]
+    public string? FirstName { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Last Name")]
+    public string? LastName { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Street Address")]
+    public string? StreetAddress { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "City")]
+    public string? City { get; set; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "State")]
+    public string? State { get; set; }
+
+    [DataType(DataType.PostalCode)]
+    [Display(Name = "Postal Code")]
+    public string? PostalCode { get; set; }
+
+    [Phone]
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Phone Number")]
+    public string? PhoneNumber { get; set; }
+}
