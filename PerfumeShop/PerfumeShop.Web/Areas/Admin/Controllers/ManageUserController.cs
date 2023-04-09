95293d8f7ee916cd6be1409ce57c8d1bc448734a
Alexander Medved <medved1991alexander@gmail.com>
2023-04-09 14:59:21 +0300

    Add get all users

---
+++ PerfumeShop/PerfumeShop.Web/Areas/Admin/Controllers/ManageUserController.cs
@@ -0,0 +1,37 @@
+﻿namespace PerfumeShop.Web.Areas.Admin.Controllers;
+
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+[Route("[area]/[controller]/[action]")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ManageUserController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+
+    public ManageUserController(
+        UserManager<AppUser> userManager,
+    RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Index(string role)
+    {
+        TempData["role"] = role;
+        TempData.Keep("role");
+        return View();
+    }
+
+    #region API CALLS
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _userManager.GetUsersInRoleAsync(TempData["role"].ToString());
+        return Json(new { data = users });
+    }
+    #endregion
+}
---
+++ PerfumeShop/PerfumeShop.Web/wwwroot/js/user-table.js
@@ -0,0 +1,61 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#userData').DataTable({
+        "ajax": {
+            "url": "/admin/manageUser/getAll"
+        },
+        "columns": [
+            { data: "id", "width": "15%" },
+            { data: "email", "width": "10%" },
+            { data: "firstName", "width": "10%" },
+            { data: "lastName", "width": "10%" },
+            { data: "state", "width": "10%" },
+            { data: "city", "width": "10%" },
+            { data: "streetAddress", "width": "10%" },
+            { data: "phoneNumber", "width": "10%" },
+            { data: "postalCode", "width": "10%" },
+            {
+                data: "id",
+                "render": function (data) {
+                    return `<div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                    <a href="/admin/manageUser/edit?id=${data}" class="btn btn-outline-primary btn-sm shadow-none">
+                    <i class="bi bi-wrench"></i> </a>
+                    <a onClick=Delete('/admin/manageUser/delete/${data}') class="btn btn-outline-danger btn-sm shadow-none"> 
+                    <i class="bi bi-trash3"></i> </a>
+					</div>`
+                },
+                "width": "5%"
+            }
+        ]
+    });
+}
+
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+
+        }
+    })
+}
