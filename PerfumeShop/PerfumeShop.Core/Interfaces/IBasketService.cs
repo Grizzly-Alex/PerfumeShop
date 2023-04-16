425497c0c42060e8bb52243b9c1bf24c5efdea9f
Alexander Medved <medved1991alexander@gmail.com>
2023-04-16 23:26:24 +0300

    Add BasketService

--- PerfumeShop/PerfumeShop.Core/Interfaces/IBasketService.cs
+++ PerfumeShop/PerfumeShop.Core/Interfaces/IBasketService.cs
@@ -2,5 +2,8 @@
 
 public interface IBasketService
 {
-
+    public Task TransferBasketAsync(string anonymousId, string userId);
+    public Task RemoveItemFromBasketAsync(int basketItemId);
+    public Task DeleteBasketAsync(int basketId);
+    public Task<Basket> AddItemToBasketAsync(string userId, int productId, int productQuantity = 1);
 }
--- PerfumeShop/PerfumeShop.Infrastructure/Services/BasketService.cs
+++ PerfumeShop/PerfumeShop.Infrastructure/Services/BasketService.cs
@@ -1,5 +1,74 @@
-﻿namespace PerfumeShop.Infrastructure.Services;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class BasketService
+namespace PerfumeShop.Infrastructure.Services;
+
+public class BasketService : IBasketService
 {
+    private const string _basketCookie = "BasketCookie";
+    private readonly IUnitOfWork<ShoppingDbContext> _shopping;
+    private readonly IUnitOfWork<CatalogDbContext> _catalog;
+    private readonly ILogger<BasketService> _logger;
+
+
+    public BasketService(
+        IUnitOfWork<ShoppingDbContext> shopping,
+        IUnitOfWork<CatalogDbContext> catalog,
+        ILogger<BasketService> logger)
+    {
+        _shopping = shopping;
+        _catalog = catalog;
+        _logger = logger;
+    }
+
+    public async Task<Basket> AddItemToBasketAsync(string userId, int productId, int productQuantity = 1)
+    {
+        var basketRepository = _shopping.GetRepository<Basket>();
+        var basket = await basketRepository.GetFirstOrDefaultAsync(
+            predicate: i => i.BuyerId == userId,
+            include: query => query.Include(b => b.Items));
+
+        if (basket is null)
+        {
+            basket = new Basket(userId);
+            basketRepository.Add(basket);
+            await _shopping.SaveChangesAsync();
+            _logger.LogInformation($"Basket was created for user ID '{userId}'.");
+        }
+
+        var basketItem = await _catalog.GetRepository<CatalogProduct>()
+            .GetFirstOrDefaultAsync(
+            predicate: product => product.Id == productId,
+            selector: product => new BasketItem(product, productQuantity));
+
+        basket.AddItem(basketItem!);
+
+        basketRepository.Update(basket);
+        await _shopping.SaveChangesAsync();
+
+        _logger.LogInformation("Basket was updated.");
+        return basket;
+    }
+
+    public async Task DeleteBasketAsync(int basketId)
+    {
+        var basketRepository = _shopping.GetRepository<Basket>();
+        var basket = await basketRepository.GetFirstOrDefaultAsync(predicate: b => b.Id == basketId)
+            ?? throw new NullReferenceException($"Basket not found with ID '{basketId}'.");
+
+        basketRepository.Remove(basket);
+
+        await _shopping.SaveChangesAsync();
+    }
+
+    public async Task RemoveItemFromBasketAsync(int basketItemId)
+    {
+        var basketRepository = _shopping.GetRepository<BasketItem>();
+        //TODO
+        await _shopping.SaveChangesAsync();
+    }
+
+    public Task TransferBasketAsync(string anonymousId, string userId)
+    {
+        throw new NotImplementedException();
+    }
 }
