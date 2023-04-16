11ca11ae2e4149d59005628f48c6f30e410ad266
Alexander Medved <medved1991alexander@gmail.com>
2023-04-16 11:55:36 +0300

    Add ShoppingDbContext

---
+++ PerfumeShop/PerfumeShop.Infrastructure/DataAccess/DbContexts/ShoppingDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace PerfumeShop.Infrastructure.DataAccess.DbContexts;
+
+public sealed class ShoppingDbContext : DbContext
+{
+    public DbSet<Basket> Baskets { get; set; }
+    public DbSet<BasketItem> BasketItems { get; set; }
+
+    public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
