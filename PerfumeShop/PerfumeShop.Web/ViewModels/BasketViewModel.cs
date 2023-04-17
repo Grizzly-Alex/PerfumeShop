7e4cb61c1ff2880a6adbd71ae9dc98d5b4a5327d
Alexander Medved <medved1991alexander@gmail.com>
2023-04-17 19:56:37 +0300

    Add BaskeViewModel

---
+++ PerfumeShop/PerfumeShop.Web/ViewModels/BasketViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PerfumeShop.Web.ViewModels;
+
+public sealed class BasketViewModel
+{
+    public int Id { get; set; }
+    public List<BasketItemViewModel> Items { get; set; }
+    public string? BuyerId { get; set; }
+}
