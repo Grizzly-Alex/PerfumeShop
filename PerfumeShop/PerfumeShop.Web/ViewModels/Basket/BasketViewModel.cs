f0fc359fad92bd5582b8143897b9ddb228397879
Alexander Medved <medved1991alexander@gmail.com>
2023-04-26 22:04:06 +0300

    Add summary view

--- PerfumeShop/PerfumeShop.Web/ViewModels/Basket/BasketViewModel.cs
+++ PerfumeShop/PerfumeShop.Web/ViewModels/Basket/BasketViewModel.cs
@@ -5,4 +5,6 @@ public sealed class BasketViewModel
     public int Id { get; set; }
     public string? BuyerId { get; set; }
     public List<BasketItemViewModel> Items { get; set; } = new();
+    public decimal? TotalProductsPrice { get; set; }
+    public decimal? TotalPrice { get; set; }
 }
