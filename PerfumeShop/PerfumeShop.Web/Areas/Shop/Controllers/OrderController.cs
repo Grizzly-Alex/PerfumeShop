75bdc0f82e25407c6db58ab92c5d63c515ae0789
Alexander Medved <medved1991alexander@gmail.com>
2023-04-27 21:43:17 +0300

    Add Index view page

---
+++ PerfumeShop/PerfumeShop.Web/Areas/Shop/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿namespace PerfumeShop.Web.Areas.Shop.Controllers;
+
+[Area("Shop")]
+//[Authorize]
+public class OrderController : Controller
+{
+	public async Task<IActionResult> Index()
+	{
+		return View();
+	}
+}
