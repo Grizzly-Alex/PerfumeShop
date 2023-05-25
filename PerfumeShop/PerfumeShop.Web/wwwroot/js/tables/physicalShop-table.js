c0ed1b386147237b0148ca3a39512fb95a68ca59
Alexander Medved <medved1991alexander@gmail.com>
2023-05-25 22:46:23 +0300

    Add edit view page

--- PerfumeShop/PerfumeShop.Web/wwwroot/js/tables/physicalShop-table.js
+++ PerfumeShop/PerfumeShop.Web/wwwroot/js/tables/physicalShop-table.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 });
 
 function loadDataTable() {
-    dataTable = $('#productData').DataTable({
+    dataTable = $('#physicalShopData').DataTable({
         "ajax": {
             "url": "/Admin/ManagePhysicalShop/GetAll"
         },
