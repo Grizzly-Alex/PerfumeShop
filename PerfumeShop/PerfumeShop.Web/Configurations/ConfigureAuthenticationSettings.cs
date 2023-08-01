c6b3377bab4d8711434f696e6ac8a457a1b478d2
Alexander Medved <medved1991alexander@gmail.com>
2023-08-02 00:16:09 +0300

    Add google and facebook nugets

--- PerfumeShop/PerfumeShop.Web/Configurations/ConfigureAuthenticationSettings.cs
+++ PerfumeShop/PerfumeShop.Web/Configurations/ConfigureAuthenticationSettings.cs
@@ -5,13 +5,15 @@ public static class ConfigureAuthenticationSettings
     public static IServiceCollection AddAuthenticationSettings(this IServiceCollection services)
     {
         services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddGoogle()
+            .AddFacebook()
             .AddCookie(options =>
             {
                 options.Cookie.HttpOnly = true;
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                 options.Cookie.SameSite = SameSiteMode.Lax;
             });
-
+        
         return services;
     }
 }
