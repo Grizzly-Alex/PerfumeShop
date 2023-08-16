f37d0b456355ea002df3a881e5b0608f55aa9d6a
Alexander Medved <medved1991alexander@gmail.com>
2023-08-16 17:18:15 +0300

    Add EmailData

---
+++ PerfumeShop/PerfumeShop.Infrastructure/EmailConfiguration/EmailData.cs
@@ -0,0 +1,41 @@
+﻿namespace PerfumeShop.Infrastructure.EmailConfiguration;
+
+public class EmailData
+{
+    #region Sender
+    public string? From { get; }
+    public string? DisplayName { get; }
+    public string? ReplyTo { get; }
+    public string? ReplyToName { get; }
+    #endregion
+
+    #region Receiver
+    public List<string> To { get; }
+    public List<string> Bcc { get; }
+    public List<string> Cc { get; }
+    #endregion
+
+    #region Content
+    public string? Subject { get; }
+    public string? Body { get; }
+    #endregion
+
+    public EmailData(
+        List<string> to,
+        string subject, string? body = null, string?
+        from = null, string? displayName = null, string?
+        replyTo = null, string? replyToName = null,
+        List<string>? bcc = null, List<string>? cc = null)
+    {
+        To = to;
+        Bcc = bcc ?? new List<string>();
+        Cc = cc ?? new List<string>();
+        From = from;
+        DisplayName = displayName;
+        ReplyTo = replyTo;
+        ReplyToName = replyToName;
+        Subject = subject;
+        Body = body;
+    }
+
+}
--- PerfumeShop/PerfumeShop.Infrastructure/EmailConfiguration/EmailSettings.cs
+++ PerfumeShop/PerfumeShop.Infrastructure/EmailConfiguration/EmailSettings.cs
@@ -2,9 +2,12 @@
 
 public sealed class EmailSettings
 {
-    public string From { get; set; }
-    public string SmtpServer { get; set; }
+    public string? DisplayName { get; set; }
+    public string? From { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public string? Host { get; set; }
     public int Port { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public bool UseSSL { get; set; }
+    public bool UseStartTls { get; set; }
 }
